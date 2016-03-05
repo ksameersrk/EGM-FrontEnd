542723305c905f30d7bdc8b67b7b6b3645db3150
Sameer Kulkarni <ksameersrk@gmail.com>
2016-03-06 02:59:17 +0530

    List view

--- app/src/main/java/com/example/jeevan/splash/ListOfPlaces.java
+++ app/src/main/java/com/example/jeevan/splash/ListOfPlaces.java
@@ -7,9 +7,12 @@ import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 /**
  * Created by ksameersrk on 5/3/16.
@@ -37,9 +40,14 @@ public class ListOfPlaces extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_of_places);
 
+        //set header
+        final TextView t = (TextView) findViewById(R.id.headerText);
+        Bundle bundle = getIntent().getExtras();
+        String tmp = bundle.getString("header");
+        t.setText(tmp);
+
         //initialize
         my_sel_items = "";
-
 
         // Find the ListView resource.
         mainListView = (ListView) findViewById(R.id.mainListView);
@@ -57,11 +65,12 @@ public class ListOfPlaces extends Activity
         mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                view.setSelected(true);
                 my_sel_items = "";
                 SparseBooleanArray a = mainListView.getCheckedItemPositions();
                 selected_places = new ArrayList<String>();
                 for (int j = 0; j < a.size(); j++) {
-                    Log.v("value", a.size() + "");
+                    Log.v("value size : ", a.size() + "");
                     if (a.valueAt(j)) {
                         my_sel_items = (String) mainListView.getAdapter().getItem(a.keyAt(j));
                         selected_places.add(my_sel_items);
@@ -70,6 +79,5 @@ public class ListOfPlaces extends Activity
                 Log.v("list", selected_places.toString());
             }
         });
-
     }
 }
