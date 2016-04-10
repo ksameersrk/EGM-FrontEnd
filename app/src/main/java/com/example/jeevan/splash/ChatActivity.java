fc9adbd1a6976a49922d2ee6f92d20b70702c167
Sameer Kulkarni <ksameersrk@gmail.com>
2016-04-10 19:29:47 +0530

    Updated chat window

--- app/src/main/java/com/example/jeevan/splash/ChatActivity.java
+++ app/src/main/java/com/example/jeevan/splash/ChatActivity.java
@@ -1,15 +1,25 @@
 package com.example.jeevan.splash;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Random;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -19,8 +29,7 @@ public class ChatActivity extends AppCompatActivity {
     private TextView mGroupName;
     private EditText mMsgText;
 
-    public static final String PREF_FILE = "PrefFile";
-    private static final String PREF_GROUP_NAME = "GroupName";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,28 +43,19 @@ public class ChatActivity extends AppCompatActivity {
 
         //get messages
         messages = getMessages();
-        messages = formatMessages(messages);
+        //messages = formatMessages(messages);
         display();
     }
 
     public void display() {
         listView = (ListView) findViewById(R.id.chat_list_view);
-        listAdapter = new ArrayAdapter<String>(
-                this,
-                android.R.layout.simple_expandable_list_item_1,
-                messages
-        );
+        listView.setAdapter(new CustomizedAdapter(this, messages));
+        updateFocus();
+    }
 
-        //set Adapter
-        listView.setAdapter(listAdapter);
-
-        listView.post(new Runnable() {
-            @Override
-            public void run() {
-                // Select the last row so it will scroll into view...
-                listView.setSelection(listAdapter.getCount() - 1);
-            }
-        });
+    public void updateFocus()
+    {
+        listView.setSelection(listView.getAdapter().getCount()-1);
     }
 
     public String getGroupName()
@@ -109,10 +109,120 @@ public class ChatActivity extends AppCompatActivity {
     //add user text
     public void addMessage(View v)
     {
-        messages.add(getUserName()+"\n"+mMsgText.getText().toString());
-        mMsgText.setText("");
-        View focusView = (ListView)findViewById(R.id.chat_list_view);
-        focusView.requestFocus();
+        if(noErrors())
+        {
+            messages.add("- : " + getUserName() + " : " + mMsgText.getText().toString());
+            mMsgText.setText("");
+            updateFocus();
+        }
+    }
+
+    //methods to check the fields
+    public boolean noErrors()
+    {
+        // Reset errors.
+        mMsgText.setError(null);
+
+        String msgText = mMsgText.getText().toString();
+
+        boolean allCorrect = true;
+        View focusView = null;
+
+        if (TextUtils.isEmpty(msgText)) {
+            mMsgText.setError("Please Enter some message");
+            focusView = mGroupName;
+            allCorrect = false;
+        }
+
+        return allCorrect;
+    }
+}
+
+
+class CustomizedAdapter extends BaseAdapter {
+
+    Context context;
+    ArrayList<String> data;
+    private static LayoutInflater inflater = null;
+    private HashMap<String, String> hm = new HashMap<>();
+
+    public CustomizedAdapter(Context context, ArrayList<String> data) {
+        // TODO Auto-generated constructor stub
+        this.context = context;
+        this.data = data;
+        inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return data.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        // TODO Auto-generated method stub
+        return data.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        View vi = convertView;
+        if (vi == null)
+            vi = inflater.inflate(R.layout.chat_list_row, null);
+
+        TextView name = (TextView) vi.findViewById(R.id.chat_person_name);
+        TextView text = (TextView) vi.findViewById(R.id.chat_person_text);
+        String details[] = data.get(position).trim().split(":");
+        Boolean currentUser = details[0].trim().equals("-");
+        Log.e("cuur", currentUser.toString());
+        String user_name = details[1].trim();
+        String user_text = details[2].trim();
+
+        name.setText(user_name);
+        text.setText(user_text);
+
+        //set random color
+        int r = 0;
+        int g = 0;
+        int b = 0;
+        if(hm.containsKey(user_name))
+        {
+            String color[] = hm.get(user_name).split(":");
+            r = Integer.parseInt(color[0]);
+            g = Integer.parseInt(color[1]);
+            b = Integer.parseInt(color[2]);
+        }
+        else
+        {
+            Random rand = new Random();
+            r = rand.nextInt();
+            g = rand.nextInt();
+            b = rand.nextInt();
+            hm.put(user_name, r+":"+g+":"+b);
+        }
+        name.setTextColor(Color.rgb(r, g, b));
+
+        if(currentUser)
+        {
+            name.setGravity(Gravity.RIGHT);
+            text.setGravity(Gravity.RIGHT);
+        }
+        else
+        {
+            name.setGravity(Gravity.START);
+            text.setGravity(Gravity.START);
+        }
+
+        return vi;
     }
 }
 
